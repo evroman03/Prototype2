b5bff5ce8c4e4cf9d4e2c7d1d02aa6c98365ee3c
gbrusatti <gbrusatti@mail.bradley.edu>
2024-03-06 16:46:21 -0600

    LevelSelector

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     // Start is called before the first frame update
     public void Menu()
